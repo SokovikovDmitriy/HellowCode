60e8af0c3222aabd9c6cc61ea1244986691d38fa
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-23 22:26:17 +0300

    n square m

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = new Random().Next(2, 100);
+Console.WriteLine (n);
+int m = new Random().Next(1, 10);
+Console.WriteLine (m);
+
+if (n == m * m)
+{
+    Console.WriteLine("Yes");
+}
+
+else
+{
+    Console.WriteLine("No");
+}
