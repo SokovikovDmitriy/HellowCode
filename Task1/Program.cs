9f46e1186201d32f7dc93571ed39ef3ca2260b01
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-23 20:51:11 +0300

    the square

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ведите число - ");
+string n = Console.ReadLine();
+double m = Convert.ToDouble (n);
+
+double x = m * m;
+Console.Write("Квадрат Вашего числа = ");
+Console.Write(x);
+
