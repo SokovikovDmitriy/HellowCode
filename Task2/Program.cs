b992b703a70447514f334d92dbc61d6ee6a89611
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-27 11:50:27 +0300

    Task2,Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Даны два числа. Показать большее и меньшее число
+Console.Write("Введите первое число - ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число - ");
+double b = Convert.ToDouble(Console.ReadLine());
+double max = a;
+double min = b;
+if (a > max) max = a;
+if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
