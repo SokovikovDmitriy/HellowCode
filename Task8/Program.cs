525202e4b986b4a44d74d6c9675482f1ded348fb
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-28 11:25:40 +0300

    For even-numbers

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Write N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int x = 1; x <= N;x++)
+{
+    if(x % 2 == 0)
+
+    {
+            Console.WriteLine(x);
+    }
+}
