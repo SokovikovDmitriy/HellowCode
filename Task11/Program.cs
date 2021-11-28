c6f264d5b1cc8e165924867c512050920ee013c3
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-28 14:10:26 +0300

    Max of 2 from random

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+//Console.Write("Ведите число - ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+int n = new Random().Next(10, 99);
+Console.WriteLine(n);
+
+int m1 = n / 10;
+Console.WriteLine($"First number - {m1}");
+
+int m2 = n % 10;
+Console.WriteLine($"Last number - {m2}");
+
+if (m1 > m2)
+{
+    Console.WriteLine($"Max namber - {m1}");
+}
+else
+{
+    Console.WriteLine($"Max namber - {m2}");
+}
