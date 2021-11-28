71a9267af91dac6841485bb74f354aebdf81b7c5
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-28 21:02:34 +0300

    task5 - function

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,1 +1,12 @@
-﻿// Выяснить является ли число чётным
+﻿// Написать программу вычисления значения функции y = f(a)
+
+static double f( double x)
+{
+    double result = ((x - 1) * (x + 1));
+    return result;
+}
+
+Console.Write(" int x - ");
+
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"y = ({x} - 1) * ({x} + 1) = {f(x)}");
