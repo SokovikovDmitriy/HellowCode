f6a05ce525a47905217150ab1ff9dfcf6f3af815
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-28 10:35:28 +0300

    Четное-нечетное

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число -  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 1)
+{
+Console.WriteLine("Число не четное");
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
