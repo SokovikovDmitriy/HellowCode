b30495d21e7a9fa0efa0564b51d438c2c6472588
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-28 14:56:23 +0300

    ex010

---
+++ Example010/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 12, 31, 4, 15, 17, 18};
+
+int n = array.Length;
+int x = 17;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == x)
+    Console.WriteLine(index);
+    index++;
+    
+}
