9b447f50e8217cc330d89d5432755441506e8615
dmitriy_s <d.a.sokovikov@gmail.com>
2021-11-28 10:58:40 +0300

    From -N to N

---
+++ Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать числа от -N до N
+
+Console.Write("A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+for (int x = a; x < b +1;x++)
+{
+    Console.WriteLine(x);
+}
