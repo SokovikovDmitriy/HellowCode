89a18235b7fd5adbeded0f1c114a7449863d6a70
dmitriy_s <d.a.sokovikov@gmail.com>
2021-12-05 14:21:48 +0300

    sdd task18

---
+++ Task18/Program.cs
@@ -0,0 +1,31 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.Clear();
+
+bool[] a = {true, false};
+bool R = true;
+
+for (int i = 0; i < a.Length; i++)
+{
+    for(int j = 0; j <a.Length; j++)
+    {
+        if( !(a[i] ||  a[j]) == (!a[i] && !a[j]))
+        {
+            Console.WriteLine($"Для x = {a[i]}, y = {a[j]} выражение верно");
+        }
+        else
+        {
+            R = false;
+            break;
+        }
+    }
+}
+
+if(R)
+{
+    Console.WriteLine("Доказано, что утверждение истинно");
+}
+else
+{
+    Console.WriteLine("Доказано что выражение ложно");
+}
