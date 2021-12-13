9c3b74c4e5773ea38abe127b31ae9c8e6431ccca
dmitriy_s <d.a.sokovikov@gmail.com>
2021-12-13 15:11:02 +0300

    endSeniore

---
+++ Task39_SumOfPaires/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент,
+//второй и предпоследний и т.д.
+Console.Clear();
+
+int[] array = new int[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+int mult = 0;
+for (int i = 0; i < array.Length / 2;)
+{
+    for (int j = array.Length-1; j > i;)
+    {
+        mult = array[i] * array[j];
+        Console.Write(array[i] +" ");
+        i++;        
+        Console.Write(array[j] +" ");
+        j--;
+        Console.WriteLine(mult);
+    }
+   
+}
+
---
+++ Task40_arrayMax-Min/Program.cs
@@ -0,0 +1,21 @@
+﻿// В Указанном массиве вещественных чисел 
+//найдите разницу между максимальным и минимальным элементом
+Console.Clear();
+
+double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+double max = array[0];
+double min = array[0];
+for (int i = 0; i < array.Length - 1; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine($"max - min =" + (max - min));
